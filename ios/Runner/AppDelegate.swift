727ce78a20484fd94c9051f2286d4ab1a7c10815
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2026-05-16 02:36:42 +0700

    chore: Remove Firebase integration and related configurations; update project dependencies and version

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,4 @@
 import Flutter
-import FirebaseCore
 import UIKit
 
 
@@ -9,8 +8,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
